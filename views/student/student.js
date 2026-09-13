77bca2f9827f84ac09402a24a5f24256984c4b70
Riad <shahrinriad196@gmail.com>
2026-09-13 19:53:56 +0600

    Add student module

---
+++ views/student/student.js
@@ -0,0 +1,14 @@
+document.getElementById("parkingBtn").addEventListener("click", function()
+{
+    window.location.href = "book-slot.php";
+});
+
+document.getElementById("bookingBtn").addEventListener("click", function()
+{
+    window.location.href = "my-bookings.php";
+});
+
+document.getElementById("logoutBtn").addEventListener("click", function()
+{
+    window.location.href = "../../controllers/logoutController.php";
+});
